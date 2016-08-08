d785d3a170e94fa75fd330b29728d0fc41e86231
Shawn Isenhart <isen0011@umn.edu>
2016-08-07 23:44:53 -0500

    Add basic BBCode to game view

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,18 @@
 class Game < ApplicationRecord
   has_many :decks, dependent: :destroy
   validates :title, presence: true, length: { minimum: 5 }
+   
+  def self.BBTEMPLATE
+    "app/views/games/_bbcode.html.erb"
+  end
+  
+  def bbcode
+    template = ERB.new File.read(Game.BBTEMPLATE)
+    template.result()
+  end
+  
+  def modbb
+    "[b][ImageID=1712641original]
+    Conquering Alien Warlord[/b]"
+  end
 end
